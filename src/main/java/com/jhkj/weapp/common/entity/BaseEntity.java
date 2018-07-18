0b73582dcf9bd6120a581ac1a011f42a2462e7be
呉真 <kuretru@gmail.com>
2018-07-18 18:11:55 +0800

    修复：DO 基类未配置 lombok 导致的问题

--- src/main/java/com/jhkj/weapp/common/entity/BaseEntity.java
+++ src/main/java/com/jhkj/weapp/common/entity/BaseEntity.java
@@ -1,10 +1,13 @@
 package com.jhkj.weapp.common.entity;
 
+import lombok.Data;
+
 import java.time.Instant;
 
 /**
  * @author 呉真 Kuretru < kuretru@gmail.com >
  */
+@Data
 public abstract class BaseEntity {
 
     protected Long id;
