4414c186a38cb2c0fafc17a8e015b15dcf0272f4
呉真 <kuretru@gmail.com>
2018-07-23 14:38:51 +0800

    新增：参数错误自定义异常

---
+++ src/main/java/com/jhkj/weapp/common/exception/InvalidParametersException.java
@@ -0,0 +1,12 @@
+package com.jhkj.weapp.common.exception;
+
+/**
+ * @author 呉真 Kuretru < kuretru@gmail.com >
+ */
+public class InvalidParametersException extends Exception {
+
+    public InvalidParametersException(String message) {
+        super(message);
+    }
+
+}
---
+++ src/main/java/com/jhkj/weapp/common/exception/MissingParametersException.java
@@ -0,0 +1,12 @@
+package com.jhkj.weapp.common.exception;
+
+/**
+ * @author 呉真 Kuretru < kuretru@gmail.com >
+ */
+public class MissingParametersException extends Exception {
+
+    public MissingParametersException(String message) {
+        super(message);
+    }
+
+}
