9d1f312500442974ea2d775b9e3ab981f47fc603
呉真 <kuretru@gmail.com>
2018-07-28 12:18:56 +0800

    新增：用户身份认证失败异常

---
+++ src/main/java/com/jhkj/weapp/common/exception/UserUnauthenticatedException.java
@@ -0,0 +1,12 @@
+package com.jhkj.weapp.common.exception;
+
+/**
+ * @author 呉真 Kuretru < kuretru@gmail.com >
+ */
+public class UserUnauthenticatedException extends Exception {
+
+    public UserUnauthenticatedException(String message) {
+        super(message);
+    }
+
+}
