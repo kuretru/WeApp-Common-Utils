7e2d86dd4039c460ee8823629e9b66d97552bdf8
呉真 <kuretru@gmail.com>
2018-08-06 21:00:45 +0800

    新增：管理员身份认证异常

---
+++ src/main/java/com/jhkj/weapp/common/exception/AdminUnauthenticatedException.java
@@ -0,0 +1,12 @@
+package com.jhkj.weapp.common.exception;
+
+/**
+ * @author 呉真 Kuretru < kuretru@gmail.com >
+ */
+public class AdminUnauthenticatedException extends Exception {
+
+    public AdminUnauthenticatedException(String message) {
+        super(message);
+    }
+
+}
