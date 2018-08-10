463363173089da60e8844454f7f13076d3c0522d
呉真 <kuretru@gmail.com>
2018-08-10 18:41:58 +0800

    新增：统一枚举类型接口规范

---
+++ src/main/java/com/jhkj/weapp/common/entity/enums/BaseEnum.java
@@ -0,0 +1,15 @@
+package com.jhkj.weapp.common.entity.enums;
+
+/**
+ * @author 呉真 Kuretru < kuretru@gmail.com >
+ */
+public interface BaseEnum {
+
+    /**
+     * 获取枚举的自定义编号
+     *
+     * @return 自定义编号
+     */
+    int getCode();
+
+}
